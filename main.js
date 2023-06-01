f8541707f16b88110eb7f685223f6d667cbd8e33
Gathan Mahesa <gthn217@gmail.com>
2023-06-01 10:19:33 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+import fragmentShader from './shaders/fragment.glsl';
+import vertexShader from './shaders/vertex.glsl';
+
+const canvasContainer = document.querySelector("#canvas-container");
+const canvas = document.querySelector("canvas");
+const HEIGHT = canvasContainer.offsetHeight;
+const WIDTH = canvasContainer.offsetWidth;
+
+const renderer = new THREE.WebGLRenderer({ antialias : true, canvas : canvas });
+renderer.setSize( WIDTH, HEIGHT );
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+camera.position.z = 15;
+
+const something = new THREE.Mesh(
+  new THREE.PlaneGeometry( 20, 20 ),
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    side : THREE.DoubleSide,
+    uniforms : {
+      color1 : { value : new THREE.Color( "orange" ) },
+      color2 : { value : new THREE.Color( "yellow" ) }
+    }
+  })
+)
+scene.add(something)
+
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera)
+}
+
+animate();
+console.log(scene)
---
+++ shaders/fragment.glsl
@@ -0,0 +1,8 @@
+uniform vec3 color1;
+uniform vec3 color2;
+
+varying vec2 vertexUV;  
+
+void main(){
+  gl_FragColor = vec4( 1.0, 1.0, 1.0 , 1.0 );
+}
